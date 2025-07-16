d24c12ca01d144f6e5b8f8b9689d9ad0233c5017
JurajMajernik <xmajernik@stuba.sk>
2025-07-16 16:09:38 +0200

    mail edit

--- jpa/src/main/java/com/data/jpa/config/MailConfig.java
+++ jpa/src/main/java/com/data/jpa/config/MailConfig.java
@@ -1,5 +1,7 @@
 package com.data.jpa.config;
 
+import java.util.Properties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,7 +12,17 @@ public class MailConfig {
 	@Bean
 	public JavaMailSender mailSender() {
 		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-		mailSender.setHost("majernik.juraj@gmail.com");
+		mailSender.setHost("smtp.gmail.com");
+		mailSender.setPort(587);
+		mailSender.setUsername("majernik.juraj@gmail.com");
+		mailSender.setPassword("");
+		
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.debug", "true");
+        
 		return mailSender;
 	}
 }
